f860e3717674e267b6149efb4d65d57b5c419dc2
David Hawley <astroverse4223@gmail.com>
2026-02-10 18:42:16 -0600

    Fixed some errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { MandelbrotRenderer } from './components/MandelbrotRenderer';
 import { ControlPanel } from './components/ControlPanel';
 import { HUD } from './components/HUD';
